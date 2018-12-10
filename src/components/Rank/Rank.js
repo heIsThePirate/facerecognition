094c7521705ecb1c04b95352d941684d3eedddde
Mohit Singh <mohitsingh1997@gmail.com>
2018-12-10 12:53:41 +0530

    adding front-end design

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Rank extends Component {
+	render(){
+		return(
+		<div className='white f3'>
+		<div>
+			{'Mohit, your current rank is...'}
+			</div>
+			<div>
+			{'#5'}
+			</div>
+		</div>
+		);
+	}
+}
+
+export default Rank;
